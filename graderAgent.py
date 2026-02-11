4afc195196a7bd26d35da0864fb31acf31402963
Krish <pkrish139@gmail.com>
2026-02-11 01:51:38 -0600

    Added new functionality to the existing endpoint

---
+++ graderAgent.py
@@ -0,0 +1,27 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+import prompts
+
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_KEY"))
+
+model = "gpt-4o"
+temperature = 0.0
+max_tokens = 1000
+
+system_prompt = prompts.system_prompt
+prompt = prompts.generate_prompt()
+
+def grader_results():
+    completion = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": prompt}
+        ],
+        temperature=temperature,
+        max_tokens=max_tokens,
+    )
+    return completion.choices[0].message.content
